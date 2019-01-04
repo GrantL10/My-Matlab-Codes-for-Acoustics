932944a0662eb9f1149b0ecb3474cec80d6186e9
Gang <38623913+aTTracT10@users.noreply.github.com>
2019-01-04 11:01:19 +0800

    Add files via upload
    The Moving Average Filter

---
+++ MovingAverage.m
@@ -0,0 +1,14 @@
+function [ y ] = MovingAverage( x, win_size )
+
+% by Dr.Li 
+
+% x is the input data 
+% y it the ouput data
+% win_size is the size of moving windows
+
+    y1=filter(ones(1,win_size/2+1)/win_size,1,x);
+    y2=filter(ones(1,win_size/2+1)/win_size,1,fliplr(x));
+    y=y1+fliplr(y2)-(1/win_size)*x;
+    
+    
+end
