cb5d608dac687ca232663cea24c83f63cbfa0afa
Gang <38623913+aTTracT10@users.noreply.github.com>
2021-06-27 16:31:55 +0800

    Update MovingAverage.m

--- MovingAverage.m
+++ MovingAverage.m
@@ -6,9 +6,9 @@ function [ y ] = MovingAverage( x, win_size )
 % y it the ouput data
 % win_size is the size of moving windows
 
-    y1=filter(ones(1,win_size/2+1)/win_size,1,x);
-    y2=filter(ones(1,win_size/2+1)/win_size,1,fliplr(x));
-    y=y1+fliplr(y2)-(1/win_size)*x;
+    y1 = filter(ones(1 ,floor(win_size/2) + 1) / win_size, 1, x);
+    y2 = filter(ones(1 ,floor(win_size/2) + 1) / win_size, 1, fliplr(x));
+    y = y1 + fliplr(y2) - (1/win_size) * x;
     
     
 end
